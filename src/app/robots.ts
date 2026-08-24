5b61fbedd2ca5274fe8e42bbb72bd854c7b5ad1e
Alwil17 <willialfred24@gmail.com>
2026-08-24 17:08:02 +0000

    Allow all routes to be indexed in robots.txt for Google Search Console

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/dashboard', '/clients', '/projects', '/entries', '/auth'],
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
